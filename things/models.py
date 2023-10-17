0d6956ab6a08a3ae68cde1d29783bd4467bed557
Omar Yahya <omar.yahya@kcl.ac.uk>
2023-10-17 01:23:03 +0100

    Add input validation for fields

--- things/models.py
+++ things/models.py
@@ -1,12 +1,19 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Thing(models.Model):
     name = models.CharField(
-        max_length=20
+        max_length=30,
+        unique=True,
+        blank=False
     )
     description = models.CharField(
-        max_length=50
+        max_length=120,
+        blank=True
     )
     quantity = models.IntegerField(
-
+        validators=[
+            MaxValueValidator(100),
+            MinValueValidator(0)
+        ]
     )
